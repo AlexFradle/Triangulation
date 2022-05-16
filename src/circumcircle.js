dc41ffba77ac2ea2d29bfe3fbba2377401bee95a
AlexFradle <alexandergoodall123@gmail.com>
2022-05-16 19:41:34 +0100

    wikipedia circumcircle equation (pain)

--- src/circumcircle.js
+++ src/circumcircle.js
@@ -1,11 +1,28 @@
 import p5 from "p5";
 import { circumcenter, circumradius } from "./utils";
 
+const otherMethod = (a, b, c) => {
+    // https://en.wikipedia.org/wiki/Circumscribed_circle#Cartesian_coordinates_2
+    const bp = [b[0] - a[0], b[1] - a[1]];
+    const cp = [c[0] - a[0], c[1] - a[1]];
+    const dp = 2 * (bp[0]*cp[1] - bp[1]*cp[0]);
+    const f = bp[0]*bp[0] + bp[1]*bp[1];
+    const g = cp[0]*cp[0] + cp[1]*cp[1];
+    const uprime = [
+        (cp[1]*f - bp[1]*g) / dp,
+        (bp[0]*g - cp[0]*f) / dp
+    ];
+    const r = Math.sqrt(uprime[0]*uprime[0] + uprime[1]*uprime[1]);
+    console.log(uprime[0] + a[0], uprime[1] + a[1], r);
+}
+
 const POINTS = [[310, 130], [220, 280], [350, 130]];
 const sketch = (p) => {
     const points = POINTS;
     let cr = circumradius(...points);
     let cc = circumcenter(...points);
+    console.log(cc, cr);
+    otherMethod(...points);
 
     p.setup = () => {
         p.createCanvas(1000, 1000);
